20550b8a76bcf658b978253693fb72afe1ee5c0e
Ali <alissaeid@live.co.uk>
2023-02-12 13:49:39 +0000

    Add structure for game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log('connected');
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectScore(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditonQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion (){
+
+}
+
+function displayDivisionQuestion(){
+
+}
